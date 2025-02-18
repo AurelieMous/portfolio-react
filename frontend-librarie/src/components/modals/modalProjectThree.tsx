eac2b98fcc18664c2ccc1676ee80793a816c8d4a
Aurélie <naoorelie@gmail.com>
2025-02-18 16:33:47 +0100

    ajout d'un nouveau projet

--- frontend-librarie/src/components/modals/modalProjectThree.tsx
+++ frontend-librarie/src/components/modals/modalProjectThree.tsx
@@ -1,5 +1,15 @@
-import {Modal, ModalBody, ModalContent, ModalFooter, ModalHeader} from "@heroui/modal";
-import {Button} from "@heroui/button";
+import {
+  Modal,
+  ModalBody,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+} from "@heroui/modal";
+import { Button } from "@heroui/button";
+import { Link } from "@heroui/link";
+import { button as buttonStyles } from "@heroui/theme";
+
+import { GithubIcon } from "@/components/icons.tsx";
 
 interface IModalProject {
   isOpen: boolean;
@@ -12,15 +22,44 @@ const ModalProjectTwo: React.FC<IModalProject> = ({ isOpen, onOpenChange }) => {
       <ModalContent>
         {(onClose) => (
           <>
-            <ModalHeader className="flex flex-col gap-1">
-              En cours
-            </ModalHeader>
-            <ModalBody>
+            <ModalHeader className="flex flex-col gap-1">PokeTeam!</ModalHeader>
+            <ModalBody className="break-words">
               <p>
-                A venir prochainement...
+                PokeTeam! est un projet personnel actuellement en développement,
+                conçu pour les fans de Pokémon souhaitant créer et gérer leur
+                propre équipe.
+              </p>
+
+              <p>
+                Développé avec React et TypeScript, l&#39;application offre une
+                expérience fluide et interactive en exploitant les capacités de
+                ce framework moderne.
+              </p>
+
+              <p>
+                PokeTeam! s&#39;appuie sur une API externe pour récupérer des
+                données Pokémon en temps réel, permettant aux utilisateurs
+                d'explorer et de sélectionner leurs créatures préférées.
+              </p>
+
+              <p>
+                Le design de l&#39;application est entièrement réalisé avec
+                Chakra UI, garantissant une interface utilisateur ergonomique,
+                responsive et esthétique.
               </p>
             </ModalBody>
             <ModalFooter>
+              <Link
+                isExternal
+                className={buttonStyles({
+                  variant: "bordered",
+                  radius: "full",
+                })}
+                href={"https://github.com/AurelieMous/poke-team"}
+              >
+                <GithubIcon size={20} />
+                Mon repo
+              </Link>
               <Button color="danger" variant="light" onPress={onClose}>
                 Fermer
               </Button>
--- frontend-librarie/src/pages/projects.tsx
+++ frontend-librarie/src/pages/projects.tsx
@@ -2,10 +2,11 @@ import { Card, CardHeader, CardBody, CardFooter } from "@heroui/card";
 import { Image } from "@heroui/image";
 import { IoLogoReact } from "react-icons/io5";
 import {FaCss3Alt, FaHtml5} from "react-icons/fa";
-import {SiNextui, SiTailwindcss, SiVite} from "react-icons/si";
+import {SiChakraui, SiNextui, SiTailwindcss} from "react-icons/si";
 import { BiLogoTypescript } from "react-icons/bi";
 import { Button } from "@heroui/button";
 import { useDisclosure } from "@heroui/modal";
+import poketeam from "../assets/image/poketeam.png"
 
 import meteoImage from "../assets/image/meteo.png";
 import portfolioImage from "../assets/image/portfolio.png";
@@ -14,14 +15,14 @@ import ModalProjectOne from "@/components/modals/modalProjectOne.tsx";
 import DefaultLayout from "@/layouts/default";
 import { title } from "@/components/primitives";
 import ModalProjectTwo from "@/components/modals/modalProjectTwo.tsx";
-//import ModalProjectThree from "@/components/modals/modalProjectThree.tsx";
+import ModalProjectThree from "@/components/modals/modalProjectThree.tsx";
 import {useMediaQueryPersonnalise} from "@/context/mediaQueryContext.tsx";
 
 export default function ProjectsPage() {
   // Déplacer les useDisclosure en dehors du ParentComponent
   const modal1 = useDisclosure();
   const modal2 = useDisclosure();
-  //const modal3 = useDisclosure();
+  const modal3 = useDisclosure();
 
   const {isDesktopOrLaptop} = useMediaQueryPersonnalise();
   // const modal4 = useDisclosure();
@@ -40,11 +41,11 @@ export default function ProjectsPage() {
               <div className="flex flex-col items items-center justify-center alig gap-2">
                 <p>Technologies :</p>
                 <div className="flex items-center justify-center gap-2">
-                  <IoLogoReact className="text-default-500 text-3xl text-violet-600" />
-                  <FaHtml5 className="text-default-500 text-3xl text-orange-600" />
-                  <SiTailwindcss className="text-default-500 text-3xl text-blue-500" />
-                  <BiLogoTypescript className="text-default-500 text-3xl text-blue-600" />
-                  <SiNextui className="text-default-500 text-2xl text-gray-950" />
+                  <IoLogoReact className=" text-3xl text-violet-600" />
+                  <FaHtml5 className=" text-3xl text-orange-600" />
+                  <SiTailwindcss className=" text-3xl text-blue-500" />
+                  <BiLogoTypescript className=" text-3xl text-blue-500" />
+                  <SiNextui className=" text-2xl text-gray-950" />
                 </div>
               </div>
             </CardHeader>
@@ -68,15 +69,14 @@ export default function ProjectsPage() {
           {/* Deuxième carte */}
           <Card className="py-4">
             <CardHeader className="pb-0 pt-2 px-4 flex-col items-center">
-              <h4 className="font-bold text-large">Météo</h4>
+              <h4 className="font-bold text-large">NaoMétéo</h4>
               <div className="flex flex-col items items-center justify-center alig gap-2">
                 <p>Technologies :</p>
                 <div className="flex items-center justify-center gap-2">
-                  <IoLogoReact className="text-default-500 text-3xl text-violet-600" />
-                  <SiVite className="text-default-500 text-3xl text-yellow-600" />
-                  <FaHtml5 className="text-default-500 text-3xl text-orange-600" />
-                  <FaCss3Alt className="text-default-500 text-3xl text-blue-500" />
-                  <BiLogoTypescript className="text-default-500 text-3xl text-blue-600" />
+                  <IoLogoReact className=" text-3xl text-violet-600" />
+                  <FaHtml5 className=" text-3xl text-orange-600" />
+                  <FaCss3Alt className=" text-3xl text-blue-500" />
+                  <BiLogoTypescript className=" text-3xl text-blue-500" />
                 </div>
               </div>
             </CardHeader>
@@ -97,12 +97,16 @@ export default function ProjectsPage() {
             </CardFooter>
           </Card>
           {/* 3eme card */}
-          {/*<Card className="py-4">
+          <Card className="py-4">
             <CardHeader className="pb-0 pt-2 px-4 flex-col items-center">
-              <h4 className="font-bold text-large">En cours</h4>
+              <h4 className="font-bold text-large">PokeTeam!</h4>
               <div className="flex flex-col items items-center justify-center alig gap-2">
                 <p>Technologies :</p>
                 <div className="flex items-center justify-center gap-2">
+                  <IoLogoReact className=" text-3xl text-violet-600" />
+                  <BiLogoTypescript className=" text-3xl text-blue-500" />
+                  <FaHtml5 className=" text-3xl text-orange-600" />
+                  <SiChakraui className=" text-3xl text-blue-300"/>
                 </div>
               </div>
             </CardHeader>
@@ -110,7 +114,7 @@ export default function ProjectsPage() {
               <Image
                 alt="Card background"
                 className="object-cover rounded-xl"
-                src="/in-progress.jpg"
+                src={poketeam}
                 width={270}
               />
             </CardBody>
@@ -121,7 +125,7 @@ export default function ProjectsPage() {
                 onOpenChange={modal3.onOpenChange}
               />
             </CardFooter>
-          </Card>*/}
+          </Card>
         </div>
       </section>
     </DefaultLayout>

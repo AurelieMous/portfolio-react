7cfa5f619396756774fdb406aac633c44950f264
Aurélie <naoorelie@gmail.com>
2025-03-06 09:20:36 +0100

    Mise en place workflow pour github page

--- frontend-librarie/vite.config.ts
+++ frontend-librarie/vite.config.ts
@@ -1,9 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tsconfigPaths from 'vite-tsconfig-paths'
+import { defineConfig } from 'vite';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  base: "./",
-  plugins: [react(), tsconfigPaths()],
+  base: '/aureliemous.github.io/',
+  build: {
+    outDir: 'dist',
+  },
 });

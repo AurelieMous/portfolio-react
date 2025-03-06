71b820a56a17227ee7861c42602ff59c5efba8bc
Aurélie <naoorelie@gmail.com>
2025-03-06 10:03:55 +0100

    Mise en place workflow pour github page : correction page blanche

--- frontend-librarie/vite.config.ts
+++ frontend-librarie/vite.config.ts
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react"
 
 export default defineConfig({
   plugins: [react()],
-  base: "/aureliemous.github.io/",
+  base: "/frontend-librarie/",
   build: {
     outDir: "dist",
   },

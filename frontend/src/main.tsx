1f1aa919d8d1debca488763b15ecbb364f5b39e3
Aurélie <naoorelie@gmail.com>
2025-03-06 16:42:39 +0100

    Mise en place workflow pour github page : correction BrowserRouter vers HashRouter

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,21 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
-
 import App from "./App.tsx";
 import { Provider } from "./provider";
 
 import "./styles/globals.css";
 import { MediaQueryProvider } from "./context/mediaQueryContext";
+import {HashRouter} from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <HashRouter basename="/">
       <Provider>
         <MediaQueryProvider>
           <App />
         </MediaQueryProvider>
       </Provider>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>,
 );

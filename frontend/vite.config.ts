9545ffbbcbb3cc410a4cdb925127255b4b16c604
Aurélie <naoorelie@gmail.com>
2025-03-06 13:22:27 +0100

    Mise en place workflow pour github page : test autre commande

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,7 +5,7 @@ import path from "path";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/portfolio-react/",
+  base: "/",
   server: {
     host: true,
     watch: {
